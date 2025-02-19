e1da75a133ca35ca2e806c9f794a08adca9e4c2a
Tuấn Anh <anh249205@gmail.com>
2025-02-19 17:04:44 +0700

    Add files via upload

---
+++ Student.sql
@@ -0,0 +1,20 @@
+﻿create database CMCUni2;
+use CMCUni2;
+
+-- Tạo bảng Students
+CREATE TABLE Students (
+    Id INT IDENTITY(1,1) PRIMARY KEY, -- ID tự động tăng
+    Name NVARCHAR(100) NOT NULL,
+    Age INT NOT NULL,
+    Class NVARCHAR(50) NOT NULL
+);
+GO
+
+INSERT INTO Students (Name, Age, Class) VALUES 
+('Nguyen Van A', 20, 'CTK44'),
+('Tran Thi B', 21, 'CTK45'),
+('Le Van C', 22, 'CTK46');
+GO
+
+SELECT * FROM Students;
+
